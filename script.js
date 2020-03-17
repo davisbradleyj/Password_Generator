081a03c9dca8f6e6b95773972bd206b60bda28fb
guachochi <davis.bradleyj@gmail.com>
2020-03-17 00:59:39 -0700

    copy working files into repo, first updates to js code to define password arrays

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var Numbers = [0,1,2,3,4,5,6,7,8,9]
+var UpperLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var LowerLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var Special = ["~","!","@","#","$","%","^","&","*","?","_","<",">","+","{","}"]
+
+alert(password)
