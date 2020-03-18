630e221370d82f41d7c894d8a09478cb0fbb6d7d
guachochi <davis.bradleyj@gmail.com>
2020-03-18 16:56:25 -0700

    update readme, begin writing js code

--- script.js
+++ script.js
@@ -13,9 +13,52 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var Numbers = [0,1,2,3,4,5,6,7,8,9]
-var UpperLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var LowerLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var Special = ["~","!","@","#","$","%","^","&","*","?","_","<",">","+","{","}"]
+var array = []
+        var numbers = [01234456789];
+        var upperLetter = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+        var lowerLetter = ["abcdefghijklmnopqrstuvwxyz"];
+        var special = ["~!@#$%^&*?_<>+{}"];
 
+
+        var charCount = prompt("Please select a character length between 8 and 128?");
+        if ((charCount < 8) || (charCount > 128)) {
+            alert("Character count must be between 8 and 128, please refresh and try again. Thank you.");
+        };
+        
+        var confNumbers = confirm("Would you like numbers?");
+        if (confNumbers = true) {
+            console.log(array);
+        } else {
+            alert("Ok, no numbers.")
+        };
+
+        var confUpperLetter = confirm("Would you like upper case letters?");
+        if (confUpperLetter = true) {
+            console.log(array);
+        } else {
+            alert("Ok, no upper case letters.")
+        };
+
+        var confLowerLetter = confirm("Would you like lower case letters?");
+        if (confLowerLetter = true) {
+            console.log(array);
+        } else {
+            alert("Ok, no lower case letters.")
+        };
+
+        var confSpecial = confirm("Would you like special characters?");
+        if (confSpecial = true) {
+            console.log(array);
+        } else {
+            alert("Ok, no special characters.")
+        };
+
+        for (var i=0; i < charCount+1; i++) {
+            var randomNumber = Math.floor(Math.random() * array.lenth);
+
+
+[A2Z]
+
+)
+  prompt("")
 alert(password)
