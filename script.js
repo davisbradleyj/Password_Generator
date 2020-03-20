9f6169750763690dc5075db0e631ac48ace99332
guachochi <davis.bradleyj@gmail.com>
2020-03-20 07:10:16 -0700

    updated js, tracking where code delivers undefined for password

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
@@ -14,54 +14,55 @@ generateBtn.addEventListener("click", writePassword);
 // create each of these as a function
 // consider while loop for the char count
 
-        // Set global variable for an array from which password characters will be chosen
-        var passRandom = []
-        var charAvail = []
-        var charCount = []
+// Set global variable for an array from which password characters will be chosen
+var passRandom = [];
+var charAvail = [];
+var charCount = [];
+var charInt = {};
+
 
 function generatePassword() {
-        // Set basic user questions to determine password length
-        var charCount = prompt("Please select a character length between 8 and 128?");
-        if ((parseInt(charCount) >= 8) && (parseInt(charCount) <= 128)) {
-            
-            return setPassword(setCharAvail(), charCount);
-
-        } else { 
-            alert("Character count must be between 8 and 128.");
-            generatePassword();
-        };
+    // Set basic user questions to determine password length
+    while ((charCount) < 8 || (charCount) > 128) {
+        charCount = prompt("Please select a character length between 8 and 128");
+        charInt = charCount
+        console.log(charInt);
+    }
+    return setPassword();
 }
 
-function setCharAvail(){
-        var confNumbers = confirm("Would you like numbers in your password?");
-        if (confNumbers === true) {
-            var charAvail = "0123456789";
-        };
+function setPassword() {
 
-        var confUpperLetter = confirm("Would you like upper case letters in your password?");
-        if (confUpperLetter === true) {
-            charAvail = charAvail + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        };
+    var confNumbers = confirm("Would you like numbers in your password?");
+    if (confNumbers === true) {
+        charAvail = "0123456789";
+    }
 
-        var confLowerLetter = confirm("Would you like lower case letters in your password?");
-        if (confLowerLetter === true) {
-            charAvail = charAvail + "abcdefghijklmnopqrstuvwxyz";
-        };
+    var confUpperLetter = confirm("Would you like upper case letters in your password?");
+    if (confUpperLetter === true) {
+        charAvail = charAvail + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
 
-        var confSpecial = confirm("Would you like special characters in your password?");
-        if (confSpecial === true) {
-            charAvail = charAvail + "~!@#$%^&*()";
-        };
+    var confLowerLetter = confirm("Would you like lower case letters in your password?");
+    if (confLowerLetter === true) {
+        charAvail = charAvail + "abcdefghijklmnopqrstuvwxyz";
+    }
 
-        return fullAray;
-};
+    var confSpecial = confirm("Would you like special characters in your password?");
+    if (confSpecial === true) {
+        charAvail = charAvail + "~!@#$%^&*?\|,_-";
+    }
 
-function setPassword(){
-            // Create loop to generate password, log password to track any issues
-            for (var i=0; i < charCount; i++) {
-                var randomNumber = Math.floor(Math.random()*charAvail.length);
-                var passChar = charAvail[randomNumber];
-                var passRandom = passRandom + passChar; 
-                console.log(passRandom);
-        };
-};
+    console.log(charAvail);
+
+    // Create loop to generate password, log password to track any issues
+    var countChar = parseInt(charInt);
+    console.log(parseInt(charInt));
+    console.log(countChar);
+    for (var i = 0; i < countChar; i++) {
+        var randomNumber = Math.floor(Math.random() * charAvail);
+        var passChar = charAvail[randomNumber];
+        var passRandom =+ passChar;
+        console.log(passRandom);
+    }
+} 
