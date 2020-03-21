b144e9b7de83339c4ae065b00e213455593f5526
guachochi <davis.bradleyj@gmail.com>
2020-03-20 19:47:00 -0700

    complete markup and comments in js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input, modification made from passwordText.value 
+// to ...textContent to allow password to populate
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -11,60 +12,46 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// create each of these as a function
-// consider while loop for the char count
-
-// Set global variable for an array from which password characters will be chosen
+// Set globals variables, and the type of content they will contain
 var passRandom = "";
 var randomChar = "";
 var charAvail = [];
 var charCount = [];
 var charInt = {};
 
-
 function generatePassword() {
     // Set basic user questions to determine password length
     while ((charCount) < 8 || (charCount) > 128) {
         charCount = prompt("Please select a character length between 8 and 128");
         charInt = charCount
-        console.log("charInt: " + charInt);
-    }
+    }  // function will return the password which will pass through to passwordText.textContent
     return setPassword();
 };
 
 function setPassword() {
-
+    // user prompts to determine password contents
     var confNumbers = confirm("Would you like numbers in your password?");
     if (confNumbers === true) {
         charAvail = "0123456789";
     }
-
     var confUpperLetter = confirm("Would you like upper case letters in your password?");
     if (confUpperLetter === true) {
         charAvail = charAvail + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-
     var confLowerLetter = confirm("Would you like lower case letters in your password?");
     if (confLowerLetter === true) {
         charAvail = charAvail + "abcdefghijklmnopqrstuvwxyz";
     }
-
     var confSpecial = confirm("Would you like special characters in your password?");
     if (confSpecial === true) {
         charAvail = charAvail + "~!@#$%^&*?\|,_-";
     }
-
-    console.log(charAvail);
-
-    // Create loop to generate password, log password to track any issues
-    console.log(charInt);
-    console.log(charCount);
+    // Create loop to generate password, with charCount determining the number of iterations, per the string built above, 
+    // and charAvail.length determining the amount of characters available to choose from by the loop
     for (var i = 0; i < charCount; i++) {
         var randomChar = Math.floor(Math.random()*charAvail.length);
         var passChar = charAvail[randomChar];
         passRandom += passChar;
-        console.log(passRandom);
-    }
-
+    } // return password to generatePassword to end both functions
     return passRandom;
 };
